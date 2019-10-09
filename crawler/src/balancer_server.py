14de03acbf942a60dd5a544adc55c9378a0e2764
Lucas <lucaspar@users.noreply.github.com>
2019-10-09 03:53:35 -0400

    simplified sockets code

--- crawler/src/balancer_server.py
+++ crawler/src/balancer_server.py
@@ -1,53 +1,30 @@
-#Author: Sophia Abraham 
-#Simulating socket communication between balancer and crawler 
-#Based off of https://stackabuse.com/basic-socket-programming-in-python/ 
+# Author: Sophia Abraham
+# Simulating socket communication between balancer and crawler
+# Based off of https://stackabuse.com/basic-socket-programming-in-python/
 
-import socket 
+import socket
 
-#create the socket 
-socket_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+PORT = 23456
+HOSTNAME = '127.0.0.1'
 
-#acquire the local hostname 
-local_hostname = socket.gethostname()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (HOSTNAME, PORT)
+print("Listening on {}:{}".format(HOSTNAME, PORT))
+sock.bind(server_address)
+sock.listen(1)
 
-#get the full hostname 
-local_full = socket.getfqdn()
+while True:
 
-#get the IP address 
-ip_address = socket.gethostbyname(local_hostname) 
+    connection, client_address = sock.accept()
 
-#printing the hostname, domain name and IP address 
-print("Working on" + str(local_hostname) + str(local_full) + "with" + str(ip_address))
-
-#bind the socket to port 23456 
-server_address = (ip_address, 23456)
-print("Starting up on %s port %s", server_address) 
-socket_1.bind(server_address) 
-
-#Listen for incoming connections 
-socket_1.listen(1) 
-
-while True: 
-    #wait for a connection 
-    print("Awaiting a connection") 
-    connection, client_address=socket_1.accept()
-
-    try: 
-        #show who connected 
-        print("Connection from", client_address) 
-
-        #receive the data in small chunks and print it 
-        while True: 
-            data = connection.recv(100) 
-            if data: 
-                #print the data ouput 
-                print(str(data)) 
-            else: 
-                #once all data is printed quit the loop 
-                print("No more data...") 
-                break 
-    finally: 
-        #Close the connection 
+    try:
+        print("Connection from", client_address)
+        while True:
+            data = connection.recv(100)
+            if data:
+                print(str(data))
+            else:
+                print("No more data...")
+                break
+    finally:
         connection.close()
-
-
