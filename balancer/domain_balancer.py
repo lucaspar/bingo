f95931bac70da9ae8b7d558487738e5aa001ecbd
Jin Huang <kiyoshi@jins-mbp-2019.dhcp.nd.edu>
2019-10-20 23:35:05 -0400

    Reorganize the structure of balancer & crawler

--- balancer/domain_balancer.py
+++ balancer/domain_balancer.py
@@ -1,25 +1,97 @@
 # Build a domain load balancer (server) to communicate with crawler
 # Author: Jin Huang
 # Initial date: 10/09/2019
+# Edited/restart: 10/20/2019
 # Ref: Sophia's code XD
 
 # Import useful libs
 import socket
+import redis
 
+#######################################
 # Define some parameters
+#######################################
+nb_crawler = 1
+nb_urls = 0
 
-# Set up the communication with the client
-PORT = 23456
-HOSTNAME = '127.0.0.1'
+#######################################
+# Define the functions
+#######################################
+def tmp_create_domain():
+    """
+    A tmp function, just make some initial domains and save them into redis
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = (HOSTNAME, PORT)
-print("Listening on {}:{}".format(HOSTNAME, PORT))
-sock.bind(server_address)
-sock.listen(1)
+    :return: a domain list, not important anyways so dont be too serious!
+    """
 
+    # Create domain list
+    init_domain = {{"url": "https://www.wikipedia.org",
+                    "status" : "000",
+                    "timestamp":"000"},
+                   {"url": "https://www.nd.edu",
+                    "status": "000",
+                    "timestamp": "000"},
+                   {"url": "https://www.quora.com",
+                    "status": "000",
+                    "timestamp": "000"}}
+
+    # TODO: Save the data into redis (How???)
+
+def get_domain_from_redis():
+    """
+    Fetch the domain from redis database
+
+    :return:
+    """
+
+    pass
+
+def receive_data_and_organize():
+    """
+
+    :return:
+    """
+
+    pass
+
+def get_balanced_urls():
+    """
+
+    :return:
+    """
+
+    pass
+
+
+#######################################
+# Main
+#######################################
+# Set up the 1st set of port and hostname: balancer receive info
+PORT_1 = 12345
+HOSTNAME_1 = '127.0.0.1'
+
+# Set up the port and hostname: crawler receive info
+PORT_2 = 23456
+HOSTNAME_2 = '127.0.0.2'
+
+# Set up the socket
+sock_1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address_1 = (HOSTNAME_1, PORT_1)
+print("Listening on {}:{}".format(HOSTNAME_1, PORT_1))
+sock_1.bind(server_address_1)
+sock_1.listen(1)
+
+# Call the function to make initial redis database
+tmp_create_domain()
+
+# Keep the socket open all the time
 while True:
-    connection, client_address = sock.accept()
+    # Get the first URLs from the database
+    # TODO: get data from redis database (HOW???)
+
+
+    # Set up the socket
+    connection, client_address = sock_1.accept()
 
     try:
         print("Connection from", client_address)
--- balancer/fake_crawler.py
+++ balancer/fake_crawler.py
@@ -5,16 +5,30 @@
 # Import useful libs
 import socket
 
-# Set up the communication with the server
-PORT = 23456
-HOSTNAME = '127.0.0.1'
 
-# TODO: Add a random URL list.
-url_list = ""
+data = ""
 
+# Set up the 1st set of port and hostname: balancer receive info
+PORT_1 = 12345
+HOSTNAME_1 = '127.0.0.1'
+
+# Set up the port and hostname: crawler receive info
+PORT_2 = 23456
+HOSTNAME_2 = '127.0.0.2'
+
+# Set up the socket
+sock_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address_2 = (HOSTNAME_2, PORT_2)
+print("Listening on {}:{}".format(HOSTNAME_2, PORT_2))
+sock_2.bind(server_address_2)
+sock_2.listen(1)
+
+# Set up the socket: step 2- crawler connect
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOSTNAME, PORT))
-    s.sendall(url_list)
+
+    # Set up the socket: step 4- crawler starts sending
+    s.sendall(data)
     data = s.recv(1024)
 
 print('Received', repr(data))
