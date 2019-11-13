d43e99826ca0740bbd7fcd3944e576b5d293a62b
sophiaabraham96 <sophia.abraham96@gmail.com>
2019-11-12 23:01:41 -0500

    fixed socket communication with domain balancer

--- crawler/src/domain_balancer.py
+++ crawler/src/domain_balancer.py
@@ -9,24 +9,25 @@ import ast
 import random
 import threading
 import time
-
+import struct
 
 
 class domain_balancer(object):
     def __init__(self):
         self.nb_crawler = 1
         self.nb_urls_init = 1
-        self.receive_size = 1024
+        self.receive_size = 4
         self.thresh_url = 0
 
         self.PORT = 23456
         self.HOSTNAME = '127.0.0.1'
-       #  self.redis_conn = redis.Redis('localhost')
+        # self.redis_conn = redis.Redis('localhost')
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def tmp_create_domain(self):
         """
         A tmp function, just make some initial domains and save them into redis
+
         :return: NA
         """
 
@@ -42,7 +43,7 @@ class domain_balancer(object):
             value = init_domain.get(key, {})
             print(key, init_domain.get(key, {}))
 
-           #  self.redis_conn.hmset(key, value)
+            # self.redis_conn.hmset(key, value)
 
     def process_metadata_str(self, metadata_str):
         """
@@ -63,6 +64,7 @@ class domain_balancer(object):
         """
         Write URLs to the URL Map with some metadata (see below).
         If URL already exists, do nothing.
+
         :parameter:
             data: de-duplicated metadata
         :return:
@@ -79,6 +81,7 @@ class domain_balancer(object):
 
     def get_socket_listen(self):
         """
+
         :return:
         """
         # Socket connection: balancer starts to listen
@@ -91,6 +94,7 @@ class domain_balancer(object):
 
     def get_socket_acceptance(self):
         """
+
         :return:
         """
 
@@ -103,11 +107,12 @@ class domain_balancer(object):
 
     def get_balanced_urls(self):
         """
+
         :return:
         """
 
         # TODO: Update the nb of URLs according to the data size in redis
-        nb_total_url = len(self.redis_conn.keys())
+        # nb_total_url = len(self.redis_conn.keys())
 
         # TODO: How to decide the rules for nb of URLs sending to each crawler??
         if nb_total_url <= self.thresh_url:
@@ -134,6 +139,7 @@ class domain_balancer(object):
 
     def create_one_thread(self, conn, add):
        """
+
        :param sock:
        :return:
        """
@@ -145,40 +151,61 @@ class domain_balancer(object):
                # TODO: Use the get_balanced_urls function in the future
                #url_list = self.get_balanced_urls()
 
-               url_list = 'https://scrapethissite.com'
-               print("here is the url list ")
-               print(url_list)
+               # url_list = self.get_one_url_for_test()
+               url_list = 'https://stackoverflow.com/'.encode()
+               # print("here is the url list ")
+               # print(url_list)
 
                # Get the size of data and send it to the crawler.
                print("Sending the size of data")
-               print(str(len(url_list)).encode())
-               conn.sendall(str(len(url_list)).encode())
+               # print(str(len(url_list)).encode())
+               # conn.sendall(str(len(url_list)).encode())
+               print(len(url_list))
+               conn.sendall(struct.pack('>I', len(url_list)))
+
 
                # Then send the URL to the crawler
                print("Sending the URL...")
-               conn.sendall(url_list.encode())
+               conn.sendall(url_list)
 
                # Socket connection: balancer receives metadata
                try:
                    # Receive the size of the data first
                    print("Receiving the size of the crawler data.")
                    data_size_str = conn.recv(self.receive_size)
-                   data_size = int(data_size_str)
+                   data_size = struct.unpack('>I', data_size_str)[0]
+                   # data_size = int(data_size_str)
                    print("Metadata size:")
                    print(data_size)
 
                    # Receive the metadata and decode
                    total_data = conn.recv(data_size)
                    str_metadata_decode = total_data.decode()
-                   print(str_metadata_decode)
+                   # print(str_metadata_decode)
 
                    # Organize the raw data received and deduplicate
                    print("Processing data and remove duplicates...")
-                   metadata = self.process_metadata_str(ast.literal_eval(str_metadata_decode))
+                   # TODO: CHANGE TO JSON.LOADS() - also this shit is broke
+                   #metadata = self.process_metadata_str(ast.literal_eval(str_metadata_decode))
 
                    # Compare the data with that in Redis and decide whether to save
                    print("Saving metadata into Redis database...")
-                   self.check_redis_and_save_data(conn=self.redis_conn, data=metadata)
+                   # self.check_redis_and_save_data(conn=self.redis_conn, data=metadata)
+
+                   print("Receiving the size of the crawler new urls.")
+                   data_size_str = conn.recv(self.receive_size)
+                   data_size = struct.unpack('>I', data_size_str)[0]
+                   # data_size = int(data_size_str)
+                   print("Urls size:")
+                   print(data_size)
+
+                   # Receive the metadata and decode
+                   total_data = conn.recv(data_size)
+                   str_new_urls_decode = total_data.decode()
+
+                   print('THE URLS')
+                   print(str_new_urls_decode)
+                   print()
 
                except Exception as e:
                    print(str(e))
